22e518c72c6edf420b50400b33aaf3816dd00722
Awan Berlian <berlianawan7@gmail.com>
2025-01-10 11:16:46 +0700

    Create swagger.js

---
+++ swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Social Media API",
+      version: "1.0.0",
+      description: "API untuk mengakses API Key dari berbagai platform sosial media seperti TikTok, Instagram, Twitter, Facebook, dan YouTube.",
+      contact: {
+        name: "awan",
+        email: "wanzbrayy010308@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:8080/api",
+      },
+    ],
+  },
+  apis: ["./server.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+module.exports = swaggerDocs;
